83e0eb70c17247509f7f216bfd752aa0eed83f75
GaiaGD <gaia.digregorio@gmail.com>
2023-02-23 10:18:56 -0500

    fixed ux

--- src/Pages/Intro.jsx
+++ src/Pages/Intro.jsx
@@ -14,8 +14,10 @@ function Intro() {
 
         <div>
             <div className="flex items-center justify-center h-screen">
-              <div className="md:w-1/3 w-full">              
+              <div className="md:w-1/3 w-full">           
+
               {userLogged
+
                 ?
                 <div>
                   <div className="">
@@ -51,34 +53,7 @@ function Intro() {
               </div>
             </div>
 
-            <div className="flex items-center justify-center h-screen">
-              <div className="md:w-1/3 w-full">
-                <div className="text-center">
-                  {/* <h3>User Logged in:</h3>
-                  {userLogged ? <h1>{userLogged.email}</h1> : <h1>You're logged out</h1>} */}
-                  {userLoggedData &&  <h2 className="capitalize text-4xl text-center">Hello {userLoggedData.username}</h2>}
-                </div>
 
-                <div className="">
-                  <img className="object-contain mx-auto w-1/6 m-16" src="src/assets/ledger-logo.svg" />
-                </div>
-                <Link to="/signup">
-                  <button className="my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" >
-                    <h3 className="uppercase font-bold text-base">Sign up</h3>
-                  </button>
-                </Link>
-                <Link to="/login">
-                  <button className="my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" >
-                    <h3 className="uppercase font-bold text-base">Login</h3>
-                  </button>
-                </Link>
-                
-                <div className="text-center p-4">
-                  <div onClick={logOut}>Log Out</div>
-                </div>
-
-              </div>
-            </div>
         </div>
   )
 
