2f72c3895a8efd0fcb78c4e4dbba81277db76f12
GaiaGD <gaia.digregorio@gmail.com>
2023-02-28 10:38:38 -0500

    fixed error logging in

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,17 +7,27 @@ import { auth } from "../utils/firebase-config"
 import { UserContext } from "../UserContext"
 
 function Login() {
-  let navigate = useNavigate();
   const {loggingIn, userLogged, handleLogin, logIn, logOut} = useContext(UserContext)
-  
-  
-  function goToChecking(){
-  // i have to wait for the userLogged value to change when authenticated or the checkingpage would go back to login
-    setTimeout(() => {
-      return navigate('/checkingBalance')
-    }, "500")
-  }
+  const [logged, setLogged] = useState('')
+  let navigate = useNavigate();
 
+  // ALSO A GOOD SOLUTION
+  // function goToChecking(){
+  // // i have to wait for the userLogged value to change when authenticated or the checkingpage would go back to login
+  //   setTimeout(() => {
+  //     return navigate('/checkingBalance')
+  //   }, "500")
+  // }
+
+  // function goToChecking(){
+  //   setLogged(userLogged)
+  // }
+
+  useEffect(() => {
+    if (userLogged !== null){
+      return navigate('/checkingBalance')
+    }
+  }, [userLogged])
 
   return (
 
@@ -34,10 +44,14 @@ function Login() {
           <input className="w-full py-4 px-8 my-2 rounded-full border-solid border-white border-2 bg-black text-base font-light" placeholder="Password" type="password" name="password" value={loggingIn.password} onChange={handleLogin} />
         </div>
         
-        <Link to="/checkingBalance" className="block my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" onClick={() => {logIn(); goToChecking()}} >
+        <div to="/checkingBalance" className="block my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" onClick={logIn} >
             <h3 className="uppercase font-bold text-base text-center">Log in</h3>
-        </Link>
-
+        </div>
+        <div className="my-8">
+          <Link to="/signup">
+              <h2 className="underline underline-offset-4 text-center">Sign in</h2>
+          </Link>
+        </div>
       </div>
     </div>
   )
--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 function PrivateRoute({userLogged}){
-    return userLogged !== null ? <Outlet /> : <Navigate to="/login" />
+    return userLogged !== null ? <Outlet /> : <Navigate to="/" />
 }
 
 export default PrivateRoute
