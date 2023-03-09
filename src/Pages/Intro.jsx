1b5ff9f5ef91b305306757ae74d94d0a15f17f20
GaiaGD <gaia.digregorio@gmail.com>
2023-03-09 10:50:45 -0500

    motion framer staggered

--- src/Pages/Intro.jsx
+++ src/Pages/Intro.jsx
@@ -10,13 +10,19 @@ import { motion } from "framer-motion"
 
 function Intro() {
   const {userLogged, userLoggedData, logOut} = useContext(UserContext)
-  
 
+  const introVariants = {
+    initial: { opacity: 0, translateX: 200 },
+    animate: { opacity: 1,
+      translateX: 0,
+      transition: { staggerChildren: 0.1, duration: 0.8 },
+    }
+  }
 
     return (
 
         <div>
-            <Loader />
+            {/* <Loader /> */}
 
             <div className="flex items-center justify-center h-screen">
               <div className="md:w-1/3 w-full">           
@@ -24,41 +30,50 @@ function Intro() {
               {userLogged
 
                 ?
-                  <motion.div
-                    initial={{ opacity: 0, x: 200 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ delay: 3, duration: 0.5 }}
-                  >
-                    <div className="">
+                  <motion.div variants={introVariants} initial="initial" animate="animate">
+                    
+                    <motion.div variants={introVariants} className="">
                       <img className="object-contain mx-auto w-2/3 m-16" src="src/assets/ledger-logotype.svg" />
-                    </div>
-                    <div className="text-center">
+                    </motion.div>
+
+                    <motion.div variants={introVariants} className="text-center">
                       <h2 className="capitalize text-4xl">Hello {userLoggedData.username}</h2>
                       <h3 className="mt-16 mb-4 mx-[25%]">What account would you like to view?</h3>
-                      <CheckingOrSaving selected={""} />
-                    </div>
+                    </motion.div>
 
-                    <div className="my-16">
+                    <motion.div variants={introVariants}>
+                      <CheckingOrSaving selected={""} />
+                    </motion.div>
+
+                    <motion.div variants={introVariants} className="my-16">
                       <h2 className="underline underline-offset-4 text-center" onClick={logOut}>Log out</h2>
-                    </div>
+                    </motion.div>
                     
                   </motion.div>
                 :
-                  <div>
-                    <div className="">
+                  <motion.div variants={introVariants} initial="initial" animate="animate">
+
+                    <motion.div  variants={introVariants}>
                       <img className="object-contain mx-auto w-2/3 m-16" src="src/assets/ledger-logotype.svg" />
-                    </div>
-                    <Link to="/signup">
-                      <button className="my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" >
-                        <h3 className="uppercase font-bold text-base">Create Account</h3>
-                      </button>
-                    </Link>
-                    <Link to="/login">
-                      <button className="my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" >
-                        <h3 className="uppercase font-bold text-base">Login</h3>
-                      </button>
-                    </Link>
-                  </div>
+                    </motion.div>
+
+                    <motion.div  variants={introVariants}>
+                      <Link to="/signup">
+                        <button className="my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" >
+                          <h3 className="uppercase font-bold text-base">Create Account</h3>
+                        </button>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div  variants={introVariants}>
+                      <Link to="/login">
+                        <button className="my-2 w-full gradient-cta p-4 rounded-full bg-origin-border solid border-2 border-transparent" >
+                          <h3 className="uppercase font-bold text-base">Login</h3>
+                        </button>
+                      </Link>
+                    </motion.div>
+
+                  </motion.div>
 
               }
               </div>
